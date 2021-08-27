18951d990a50b50a36e9b4d1e1e401ae72da036a
vishnuh655 <vishnuh655@gmail.com>
2021-08-27 12:29:58 +0530

    Changes

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from "next";
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
 
-const NewsPage: NextPage = () => (
-  <div>
-    <h1>Quizzy App - News Page</h1>
-  </div>
-);
-
-export default NewsPage;
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
